e3dc5faf787339875ad2d3be56e3d2eaa4bd38e0
Song Hyun Kim(Maria) <globalkmaria@gmail.com>
2023-01-30 02:43:42 +0900

    add Layout

---
+++ src/views/Layout/index.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react";
+import { Outlet } from "react-router-dom";
+import { GrLanguage } from "react-icons/gr";
+import { BsFonts } from "react-icons/bs";
+import "./style.scss";
+
+function Layout(): ReactNode {
+  return (
+    <div className="layout__wrapper">
+      <div className="layout__container">
+        <nav>
+          <div className="nav__left">
+            <div className="button--small">
+              <GrLanguage />
+            </div>
+            <div className="button--small">
+              <BsFonts />
+            </div>
+          </div>
+          <div className="nav__right"></div>
+        </nav>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
