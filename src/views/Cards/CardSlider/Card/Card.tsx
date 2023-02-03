5f56628053a86627e860e988c0a360cf10c168e2
Song Hyun Kim(Maria) <globalkmaria@gmail.com>
2023-02-04 03:17:25 +0900

    Bugfix : if !== first card -> unClickable

--- src/views/Cards/CardSlider/Card/Card.tsx
+++ src/views/Cards/CardSlider/Card/Card.tsx
@@ -12,12 +12,12 @@ export function Card({ onCardClick, text, count, id }: CardProps) {
   const [displayNone, setDisplayNone] = useState<boolean>(false);
   const [fadeOut, setFadeOut] = useState<boolean>(false);
   const direction = id % 2 === 0 ? "left" : "right";
-  const order = id - count;
-  const cardPositionCss = order >= 0 ? `card--${order % 5}` : "";
+  const order = (id - count) % 5;
+  const cardPositionCss = order >= 0 ? `card--${order}` : "";
 
   const onClick = (): void => {
     if (fadeOut) return;
-
+    if (order !== 0) return;
     onCardClick();
     setFadeOut(true);
     setTimeout(() => {
