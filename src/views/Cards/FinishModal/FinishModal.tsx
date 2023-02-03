b11e177e869b94d0b0d35b85cb91be76fb36b9b6
Song Hyun Kim(Maria) <globalkmaria@gmail.com>
2023-02-04 05:08:40 +0900

    Add: Keep text - finish modal

--- src/views/Cards/FinishModal/FinishModal.tsx
+++ src/views/Cards/FinishModal/FinishModal.tsx
@@ -1,14 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./FinishModal.module.scss";
 import { MdOutlineCelebration } from "react-icons/md";
 import { isKRAtom } from "../../../store/card";
 import { useRecoilValue } from "recoil";
+import { MdCheckBoxOutlineBlank, MdCheckBox } from "react-icons/md";
+import { resetLocal, resetTextLocal } from "../../../utils/local";
 
 export function FinishModal() {
+  const navigate = useNavigate();
+  const [keepText, setKeepText] = useState(true);
   const isKR = useRecoilValue(isKRAtom);
   const goBackHomeText = isKR ? "홈으로 돌아가기" : "Go back home";
   const CongratsMessage = isKR ? KRCongratsMessage : ENCongratsMessage;
+  const CheckboxIcon = keepText ? MdCheckBox : MdCheckBoxOutlineBlank;
+  const KeepTextMessage = isKR
+    ? "글을 유지하시겠습니까?"
+    : "Do you want to keep the text?";
+
+  const onHomeClick = () => {
+    if (!keepText) {
+      resetTextLocal();
+    }
+    resetLocal();
+    navigate("/");
+  };
+
   return (
     <div className={styles["finish-modal"]}>
       <div className={styles["finish-modal__container"]}>
@@ -18,9 +35,16 @@ export function FinishModal() {
         <span className={styles.text}>
           <CongratsMessage />
         </span>
-        <Link to={"/"} className="button button--big">
+        <div
+          className={styles["keep-text"]}
+          onClick={() => setKeepText((prev) => !prev)}
+        >
+          <CheckboxIcon className={styles.icon} />
+          <span className={styles["keep-text-message"]}>{KeepTextMessage}</span>
+        </div>
+        <button onClick={onHomeClick} className="button button--big">
           {goBackHomeText}
-        </Link>
+        </button>
       </div>
     </div>
   );
